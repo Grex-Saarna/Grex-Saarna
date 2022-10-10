329758d3648d3a30a071324bd87bb4a45e0f7a69
Grex-Saarna <115462239+Grex-Saarna@users.noreply.github.com>
2022-10-10 13:19:30 +0300

    Add files via upload

---
+++ kilpkonn01.py
@@ -0,0 +1,45 @@
+#Gregori Saarna
+#10.10.22
+#Harjutame kilpkonna
+import turtle
+
+#tekitame akna
+aken = turtle.Screen()
+aken.setup(300,300)
+
+#kilpkonna loomine
+k1 = turtle.Turtle()
+k2 = turtle.Turtle()
+k3 = turtle.Turtle()
+k4 = turtle.Turtle()
+k5 = turtle.Turtle()
+k6 = turtle.Turtle()
+k7 = turtle.Turtle()
+k8 = turtle.Turtle()
+#värvide lisamine
+k1.color("lime")
+k2.color("red")
+k3.color("orange")
+k4.color("blue")
+k5.color("blue")
+k6.color("lime")
+k7.color("red")
+k8.color("orange")
+k1.lt(0)
+k1.fd(100)
+k2.lt(45)
+k2.fd(100)
+k3.lt(90)
+k3.fd(100)
+k4.lt(135)
+k4.fd(100)
+k5.lt(180)
+k5.fd(100)
+k6.lt(225)
+k6.fd(100)
+k7.lt(270)
+k7.fd(100)
+k8.lt(315)
+k8.fd(100)
+
+
