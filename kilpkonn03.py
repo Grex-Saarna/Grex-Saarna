2dc09ee9c911bb071f99885fe896c3a3beeabd12
Grex-Saarna <115462239+Grex-Saarna@users.noreply.github.com>
2022-10-10 14:04:40 +0300

    Add files via upload

---
+++ kilpkonn03.py
@@ -0,0 +1,34 @@
+#Gregori Saarna
+#10.10.22
+#harjutus02
+import turtle
+
+#akna seaded
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.title("Ülesanne 3 - GS")
+
+k1 = turtle.Turtle()
+for i in range(5):
+    k1.color("blue")
+    #k1.lt(90)
+    k1.fd(50)
+    k1.lt(90)
+    k1.fd(100)
+    k1.lt(90)
+    k1.fd(100)
+    k1.lt(90)
+    k1.fd(100)
+    k1.lt(90)
+    k1.fd(50)
+    k1.rt(72)
+    
+    
+    
+    
+    
+    
+    
+    
+
+turtle.exitonclick()
