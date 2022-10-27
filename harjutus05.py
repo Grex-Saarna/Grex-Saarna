9196ff5b1163d2e22ee10f273fa1b58129e46ffa
Grex-Saarna <115462239+Grex-Saarna@users.noreply.github.com>
2022-10-27 09:42:55 +0300

    Add files via upload

---
+++ harjutus05.py
@@ -0,0 +1,70 @@
+#Gregori Saarna
+#17.10.2022
+#harjutus05
+import random
+
+
+
+"""
+#vanused
+Vanused = [12,10,11,13,14,15,16,17,18]
+print("************")
+print("**********")
+print("***********")
+print("*************")
+print("**************")
+print("***************")
+print("****************")
+print("*****************")
+print("******************")
+
+     
+
+
+Vanus = [12,34,16,17,18,133,95,36,64]
+
+print(f"Vanem {max(Vanus)}")
+print(f"Noorem {min(Vanus)}")
+print(f"Vanuste summa {sum(Vanus)}")
+print(f"Keskmine vanus {sum(Vanus)/len(Vanus)}")
+
+
+
+#duplikaadid
+
+opilased = ["Juhan","Kati","Mario","Mario","Mati","Mati"]
+puh_opilased = []
+for i in opilased:
+    if i not in puh_opilased:
+        puh_opilased.append(i)
+        
+print(puh_opilased)
+
+#õpilased
+jrk = 1
+opilased = ["Juhan","Kati","Maarja","Jamal","Juan"]
+for i in opilased:
+    print(f"{jrk}. {i}")
+    jrk+=1
+#küsi millist tahab muuta INT-korrigeerimine
+nr = int(input("Vali nime mida muuta: "))
+nr-=1
+#milleks muudab 
+opilane = input("Milleks muudate uwu: ")
+#tee muudatus
+del opilased[nr]
+opilased.insert(nr, opilased)
+
+print(opilased)
+print("-------------------------------------------------------------------")
+
+
+#nimede lisamine loendisse
+nimed = []
+for i in range(5):
+    nimi = input("lisa nimi loendisse: ")
+    nimed.append(nimi)
+nimed.sort()    
+print(nimed)
+print(f"viimati lisatud nimi: {nimed[-1]}")
+"""
