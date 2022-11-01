ee5b1551b01713f812ed59710ef1455afe21e8e4
Grex-Saarna <115462239+Grex-Saarna@users.noreply.github.com>
2022-11-01 09:56:01 +0200

    Add files via upload

---
+++ harjutus 09.py
@@ -0,0 +1,20 @@
+#Gregori Saarna
+#01.11.22
+#Harjutus09
+import locale
+import datetime
+
+#Tekita tänane kuupäev ning esita kujul 16. June 2016
+aeg = datetime.datetime.now()
+print(aeg.strftime("%d. %B %y"))
+
+
+#kuva eestistatud kuupäev kujul 16. juuni 2016
+locale.setlocale(locale.LC_ALL, "et_EE")
+print(aeg.strftime("%d. %B %y"))
+
+
+#eralda isikukoodiust sünnikuupäev ja leia isiku vanus
+ik="50601150853"
+sp = datetime.date(int("20"+ik[1:3]),int(ik[3:5]),int(ik[5:7]))
+print(sp)
