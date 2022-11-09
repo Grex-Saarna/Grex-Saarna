0860839f4e35215bc1b4abd1cc82f639c563b0b7
Grex-Saarna <115462239+Grex-Saarna@users.noreply.github.com>
2022-11-09 10:01:26 +0200

    Add files via upload

---
+++ PythonKT.py
@@ -0,0 +1,116 @@
+#Gregori Saarna
+#09.11.22
+#Iseseisev töö
+import random
+import math
+
+fail = open("rebased.txt", encoding="UTF-8")
+
+vastused= []
+aastad = [2011,2012,2013,2014,2015,2016,2017,2018,2019]
+for rida in fail:
+    vasuvoetud.append(int(rida))
+aasta = int(input("Mis aastat soovid? "))
+index= aastad.index(aasta)
+print(index)
+
+
+
+
+
+
+print(vastused)
+
+
+file.close()
+
+"""
+#Õunad
+m= int(input("Mitu pöialpoissi tahad õunu? "))
+u=0
+for i in range(m):
+    n=random.randint(1,2)
+    print(n)
+    u+=n
+summa=14-u
+print(f"Lumivalgekesele jäi {summa} ")
+
+
+
+#taringud
+taringud = int(input("mitu taringut tahad? "))
+for i in range(taringud)
+    print(random.randint(1,6))
+
+
+
+
+
+
+
+
+#Male
+nisuterad=0.5
+a=int(input("Sisestage täisärv"))
+i = 0
+while i <a:
+    i += 1
+    
+    nisuterad*=2
+print(nisuterad)
+
+
+
+
+#Murelikud lapsevanemad
+
+ringid = int(input("mitu ringi jooksid"))
+ring = 0
+while ring  ringid:
+    ring +=1
+    if ring %2 == 0:
+        porgand+=ring
+print(f"porgandite koguarv on {porgand}")
+    
+
+
+
+
+
+
+#äratus
+
+aratus = int(input("mitu inimest on bussis: "))
+for i in range(aratus):
+    print("tõuse ja sära")
+
+
+
+#Bussid
+
+inimesed = int(input(" Inimeste arv: "))
+kohad = int(input(" kohtade arv: "))
+busse_vaja = inimesed/kohad
+print(f"Busse on vaja {math.ceil(busse_vaja)}")
+jaak = inimesed%kohad
+if jaak==0:
+    print(f"Viimases bussis on inimesi {kohad}") 
+else:
+    print(f"Viimases bussis on inimesi {jaak}")
+
+
+
+#Tervitus
+print("Tere Maailm!")
+
+#Aasta liblikas
+aasta = 2020
+liblikas = "teelehe-mosaiikliblikas"
+lause_keskosa = ". aasta liblikas on "
+lause = str(aasta)+lause_keskosa+liblikas
+print(lause)
+
+
+#Pilved
+pilv = int(input("Kuiu palju on pilvede aluse kõrgus (km)" ))
+"""
